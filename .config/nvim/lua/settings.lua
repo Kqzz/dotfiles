a6805f4339c7f61e979c361bd9406672bbfcfe6b
kqzz <48300938+Kqzz@users.noreply.github.com>
2023-08-28 21:52:23 -0400

    swap j/k w/ gj/gk

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -15,3 +15,8 @@ set.number = true
 
 vim.g.mapleader = ' '
 
+vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true })
+vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true })
+vim.api.nvim_set_keymap('n', 'gj', 'j', { noremap = true })
+vim.api.nvim_set_keymap('n', 'gk', 'k', { noremap = true })
+
